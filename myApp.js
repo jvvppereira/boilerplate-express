872581e19c6146679f99ce07827538a78cb89fd4
João Vítor <joao.paes@philips.com>
2022-02-15 19:43:05 -0300

    Added validation based on environment option

--- myApp.js
+++ myApp.js
@@ -14,7 +14,13 @@ app.get("/", function(req, res) {
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', function(req, res) {
-  res.json({"message": "Hello json"});
+  let message =  "Hello json";
+  
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    message = message.toUpperCase();
+  }
+  
+  res.json({"message": message});
 });
 
 
