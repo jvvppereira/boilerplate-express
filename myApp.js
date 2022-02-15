cb329408e5dbfef207c99abf5cd9effdbd2c511a
João Vítor <joao.paes@philips.com>
2022-02-15 19:36:58 -0300

    fix to provide parameters req and res

--- myApp.js
+++ myApp.js
@@ -13,7 +13,7 @@ app.get("/", function(req, res) {
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/json', function() {
+app.get('/json', function(req, res) {
   res.json({"message": "Hello json"});
 });
 
