8d8c9d4e671a72c930650062d3a3e1cbb83483d9
João Vítor <joao.paes@philips.com>
2022-02-15 20:18:19 -0300

    Added a middleware to run proccess before continue

--- myApp.js
+++ myApp.js
@@ -13,13 +13,18 @@ app.use('/', function (req, res, next) {
   next();
 });
 
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.send({time: req.time});
+});
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 app.use('/public', express.static(__dirname + '/public'));
-
-
 
 app.get('/json', function(req, res) {
   let message =  "Hello json";
