fc655e618137cd6d881810f5f3ca165c427af703
João Vítor <joao.paes@philips.com>
2022-02-24 21:58:23 -0300

    adding route params

--- myApp.js
+++ myApp.js
@@ -21,6 +21,14 @@ app.get('/now', function(req, res, next) {
   res.send({time: req.time});
 });
 
+
+app.get("/:word/echo", function(req, res) {
+  const  { word } = req.params;
+  
+  res.json({echo: word});
+});
+
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
